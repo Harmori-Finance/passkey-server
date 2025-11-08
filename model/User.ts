ef0a33a121ecb76b8d409d16e1c69b00a0c568f2
congktx <buicongvip@gmail.com>
2025-11-08 23:00:05 +0700

    update

--- model/User.ts
+++ model/User.ts
@@ -2,11 +2,13 @@ import mongoose from "mongoose";
 
 interface IUser {
   id: string;
+  username: string;
   currentChallenge?: string;
 }
 
 export const UserSchema = new mongoose.Schema<IUser>({
-  id: { type: String, required: true },
+  id: { type: String },
+  username: { type: String },
   currentChallenge: { type: String }
 })
 
